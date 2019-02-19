f652783bd8d6abf51ce21809fd69e0ca2346c92a
sk9647f <36513930+sk9647f@users.noreply.github.com>
2019-02-19 19:03:08 +0000

    Add files via upload

---
+++ Play.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+
+public class Play
+
+{
+    string title;
+    string genre;
+    string description;
+    string date;
+    string time;
+    //have to use datetime function to switch from a string into a formatted datetime
+    string ticketsAvailable;
+
+    
+    float duration;
+    int ticketQuantity;
+
+
+
+    public addPlay()
+
+	{
+	
+	}
+
+    public editPlay()
+    {
+
+    }
+
+    public deletePlay()
+    {
+
+    }
+
+    public displayReview()
+    {
+
+    }
+
+}
