047db0b89c937170b72ff7c93687502a42eb8d07
Vane1997 <47599381+Vane1997@users.noreply.github.com>
2019-02-19 22:33:09 +0000

    Add files via upload

---
+++ Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Order
+{
+    int orderNo;
+    string orderDate;
+    int orderTotal;
+    string ticketsType;
+
+
+    public trackOrder()
+    {
+
+    }
+
+    public cancelOrder()
+    {
+
+    }
+
+    public displayOrderSummary()
+    {
+
+    }
+
+}
