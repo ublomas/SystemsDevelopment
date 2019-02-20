b0865da22267777cd3c7232f8075064d8af661a5
sk9647f <36513930+sk9647f@users.noreply.github.com>
2019-02-20 10:10:32 +0000

    Update Play.cs

--- Play.cs
+++ Play.cs
@@ -16,28 +16,42 @@ public class Play
     
     float duration;
     int ticketQuantity;
-
-
-
-    public addPlay()
-
-	{
 	
-	}
+	// Might not need any full class variables if arguments are given in by the main. As this class doesn't create any objects with these varialbes, only pushes them between the code and database.
+	//
 
-    public editPlay()
+
+   //Unsure on what functionality can be implemented without a linked database being created before hand
+	
+    public addPlay(string addTitle, string addDate)
     {
-
+	//pseudocode
+	ADD to database IN table Plays(addTitle, addDate, *Other variables*
     }
 
-    public deletePlay()
+    public editPlay(string editTitle, string editDate)
     {
-
+    //Unsure on how the edit will work. Will we force the user into editting the entire file or just certain lines.	
+    //For example will this pull up the entire row for a certain play and the user edits it all and re inputs it.
+    //Or will the user be able to simply specify what fields they want to alter within the ui and it becomes a replace rather than reinsert.
     }
 
-    public displayReview()
+    public deletePlay(string deleteTitle, string deleteDate)
     {
-
+	DELETE ALL IN table Plays where Title = edit Title && where Date = editDate
+		//May need more identifiers to prevent clashes
     }
 
+    public displayReview(string readTitle, string readDate)
+    {
+	SELECT Reviews 
+		When Title = readTitle
+		When Date = readDate
+	from Plays
+		
+    }
+
+	
+   //Using both the title and its date as an identifier 
+	
 }
