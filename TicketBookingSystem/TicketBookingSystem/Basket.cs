0438ef045ca49cffaac9ca6f3a0c7df49ad1fce0
ublomas <47817943+ublomas@users.noreply.github.com>
2019-03-08 15:11:22 +0000

    Update to Basket class
    Allows the form to send ticket selections to the database basket sheet

--- TicketBookingSystem/TicketBookingSystem/Basket.cs
+++ TicketBookingSystem/TicketBookingSystem/Basket.cs
@@ -6,13 +6,45 @@ using System.Threading.Tasks;
 
 namespace TicketBookingSystem
 {
-    class Basket
+     abstract class Basket
+    {
+        double basketTotal;
+        int cost, quantity;
+        string play, user;
+        DateTime date;
+        public abstract void AddToOrder(string user, string play, int cost, int qauntity, DateTime date, string ticketType);
+        public abstract void RemoveFromBasket();
+    }
+
+    class ChosenPlays : Basket
     {
 
-        double basketTotal;
-        int ticketsInBasket;
-        List<string> basketItems;
-        string playName;
+        public override void AddToOrder(string user, string play, int cost, int quantity, DateTime date, string ticketType)
+        {
+
+            //id = customerID;
+            //add = address;
+
+            //needs to write to database 
+            string connString;
+            connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = C:\Users\user\Desktop\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+
+            OleDbConnection myConnection = new OleDbConnection(connString);
+
+            OleDbCommand myCommand = new OleDbCommand("INSERT INTO Basket (UserID, PlayChosen, Price, Quantity, PlayDate, TicketType) VALUES (?, ?, ?, ?, ?, ?)", myConnection);
+
+            myCommand.Parameters.AddWithValue("@UserID", user);
+            myCommand.Parameters.AddWithValue("@PlayChosen", play);
+            myCommand.Parameters.AddWithValue("@Price", cost);
+            myCommand.Parameters.AddWithValue("@Quantity", quantity);
+            myCommand.Parameters.AddWithValue("@PlayDate", date);
+            myCommand.Parameters.AddWithValue("@TicketType", ticketType);
+
+            myConnection.Open();
+            myCommand.ExecuteNonQuery();
+            myConnection.Close();
+
+        }
 
         public void DisplayCurrentTotal()
         {
